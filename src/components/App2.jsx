793ceaf7dac921e58b55d985e142e3d7d45d3d3b
Henry Taing <hktaing@gmail.com>
2020-10-24 14:23:49 -0700

    Differentiate App and App2

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -2,20 +2,9 @@ import React, {useState} from 'react';
 import '../styles.css';
 
 function App2 () {
-    const [count, setCount] = useState(0);
-    const array = [1,2,3,4,5];
-    const test = () => {
-        const newCount = count + 1;
-        setCount(newCount);
-    };
     return (
         <>
-            Count: {count}
-            {array.map((element, index) => 
-            <section className='card'>
-                App 2! {element}
-                <button onClick={test}>+</button>
-            </section>)}
+            This is application 2
         </>
     )
 }
