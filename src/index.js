59cb73054167b4cff109c4d2bb7e6442a921f00a
Henry Taing <hktaing@gmail.com>
2020-10-25 14:09:23 -0700

    Save merged changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './components/App.jsx';
-=======
 import MainContainer from './components/MainContainer.jsx'
->>>>>>> 793ceaf7dac921e58b55d985e142e3d7d45d3d3b
 
 ReactDOM.render(<MainContainer />, document.getElementById('root'));
