b1bbf753278ee2ea8d26c73d4418133b0fe29c95
Henry Taing <hktaing@gmail.com>
2020-11-02 10:27:56 -0800

    Migrate web page to Redux application

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import App from './App.jsx'
-import App2 from './App2.jsx'
-import ItemsAndGroups from './ItemsAndGroups'
+import App from '../ReduxComponents/App.jsx'
+// import App from './App.jsx'
+// import App2 from './App2.jsx'
+// import ItemsAndGroups from './ItemsAndGroups'
 import '../styles.css';
 
 function MainContainer () {
-    let app = <ItemsAndGroups />;
-    // app = (Math.floor(Math.random() * 99) % 2 === 0) ? <App /> : <App2 />
+    let app = <App />;
     return (
         <>
             {app}
