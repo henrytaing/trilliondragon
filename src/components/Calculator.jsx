e0f4dc499e76a1edb8db9dca73da21c70f28c93f
Henry Taing <hktaing@gmail.com>
2020-11-07 18:49:15 -0800

    Update button colors to match iPhone

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,25 +6,47 @@ function Calculator () {
     const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', 
                     '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
     const updateNum = (symbol) => {
-        setNumber(number + symbol);
+        setNumber(number + symbol)
     }
     const createButton = (contents) => {
-        if(contents === '0') return (
-            <button className="calculator-button-0" onClick={() => updateNum(contents)}>{contents}</button>
-        )
-        return (
-            <button className="calculator-button" onClick={() => updateNum(contents)}>{contents}</button>
-        )
+        const orange =  <button className="calculator-button-orange" onClick={() => updateNum(contents)}>{contents}</button>
+        const lightgray = <button className="calculator-button-lightgray" onClick={() => updateNum(contents)}>{contents}</button>
+        switch(contents) {
+            case '0':
+                return (
+                    <button className="calculator-button-0" onClick={() => updateNum(contents)}>{contents}</button>
+                );
+            case '÷':
+                return orange;
+            case 'x':
+                return orange;
+            case '-':
+                return orange;
+            case '+':
+                return orange;
+            case '=':
+                return orange;
+            case 'AC':
+                return lightgray;
+            case '+/-':
+                return lightgray;
+            case '%':
+                return lightgray;
+            default: 
+                return (
+                    <button className="calculator-button" onClick={() => updateNum(contents)}>{contents}</button>
+                )
+        }
     }
     return (
         <>
             <div className="container">
                 <span id="title">Calculator</span>
-                <div class="calculator-container">
-                    <div class="numbers-display">
+                <div className="calculator-container">
+                    <div className="numbers-display">
                         {number}
                     </div>
-                    <div class="buttons-container">
+                    <div className="buttons-container">
                         {buttons.map(button => {
                             return createButton(button);
                         })}
